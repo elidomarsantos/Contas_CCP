f5d3f421152810ff41880a2ccf778211393ab9e6
Elidomar Luiz dos Santos <elidomarsantos@gmail.com>
2023-02-16 22:56:09 -0300

    upddssss

--- contas/forms.py
+++ contas/forms.py
@@ -22,7 +22,7 @@ class Form_Gerais(forms.ModelForm):
         model = Gerais
         fields = '__all__'  
         widgets = {
-            'data_do_Fechamento1': forms.DateInput(format=('%Y-%m-%d'), attrs={'class':'form-control', 'placeholder':'Select Date','type': 'date'})
+            'data_do_Fechamento': forms.DateInput(format=('%Y-%m-%d'), attrs={'class':'form-control', 'placeholder':'Select Date','type': 'date'})
             
         }        
               
